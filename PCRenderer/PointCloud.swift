bfdb68f1efba02c8b157fb0adb94f085880733d6
Nischal Hada <hadanischal@gmail.com>
2019-11-25 21:26:01 +1100

    Updated swift version to 5.1.2, updated gitignore and added missing file

--- PCRenderer/PointCloud.swift
+++ PCRenderer/PointCloud.swift
@@ -9,24 +9,24 @@
 import SceneKit
 
 @objc class PointCloud: NSObject {
-    
-    var n : Int = 0
-    var pointCloud : Array<SCNVector3> = []
-    
+
+    var n: Int = 0
+    var pointCloud: Array<SCNVector3> = []
+
     override init() {
         super.init()
-        
+
         let file: String = "bun_zipper_points.ply"
         self.n = 0
-        var x, y, z : Double
-        (x,y,z) = (0,0,0)
-        
+        var x, y, z: Double
+        (x, y, z) = (0, 0, 0)
+
         // Open file
         if let path = Bundle.main.path(forResource: file, ofType: "txt") {
             do {
                 let data = try String(contentsOfFile: path, encoding: .ascii)
                 var myStrings = data.components(separatedBy: "\n")
-                
+
                 // Read header
                 while !myStrings.isEmpty {
                     let line = myStrings.removeFirst()
@@ -38,33 +38,32 @@ import SceneKit
                         break
                     }
                 }
-                
-                pointCloud = Array<SCNVector3>(repeating: SCNVector3(x:0,y:0,z:0), count: n)
-                
+
+                pointCloud = Array<SCNVector3>(repeating: SCNVector3(x: 0, y: 0, z: 0), count: n)
+
                 // Read data
                 for i in 0...(self.n-1) {
                     let line = myStrings[i]
                     x = Double(line.components(separatedBy: " ")[0])!
                     y = Double(line.components(separatedBy: " ")[1])!
                     z = Double(line.components(separatedBy: " ")[2])!
-                    
+
                     pointCloud[i].x = Float(x)
                     pointCloud[i].y = Float(y)
                     pointCloud[i].z = Float(z)
                 }
-                NSLog("Point cloud data loaded: %d points",n)
+                NSLog("Point cloud data loaded: %d points", n)
             } catch {
                 print(error)
             }
         }
-        
+
     }
-    
-    
+
     public func getNode() -> SCNNode {
         let points = self.pointCloud
-        var vertices = Array(repeating: PointCloudVertex(x: 0,y: 0,z: 0,r: 0,g: 0,b: 0), count: points.count)
-        
+        var vertices = Array(repeating: PointCloudVertex(x: 0, y: 0, z: 0, r: 0, g: 0, b: 0), count: points.count)
+
         for i in 0...(points.count-1) {
             let p = points[i]
             vertices[i].x = Float(p.x)
@@ -74,12 +73,12 @@ import SceneKit
             vertices[i].g = Float(1.0)
             vertices[i].b = Float(1.0)
         }
-        
+
         let node = buildNode(points: vertices)
         NSLog(String(describing: node))
         return node
     }
-    
+
     private func buildNode(points: [PointCloudVertex]) -> SCNNode {
         let vertexData = NSData(
             bytes: points,
@@ -112,7 +111,7 @@ import SceneKit
             bytesPerIndex: MemoryLayout<Int>.size
         )
         let pointsGeometry = SCNGeometry(sources: [positionSource, colorSource], elements: [elements])
-        
+
         return SCNNode(geometry: pointsGeometry)
     }
 }
